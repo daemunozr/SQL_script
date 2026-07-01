642c986560a9bf1e35da67d001e4ce3f4d10e7fc
daemunozr <dae.munozr@duocuc.cl>
2026-06-30 20:55:40 -0400

    añadido informacion bonita

---
+++ scripts/sandbox.sql
@@ -0,0 +1,104 @@
+--- info
+SELECT table_name From user_tables where upper(table_name) like '%EMP%';
+SELECT table_name From user_tables where upper(table_name) like '%ARRI%';
+SELECT table_name From user_tables where upper(table_name) like '%MOTO%';
+SELECT table_name From user_tables where upper(table_name) like '%CLIENTE%';
+SELECT table_name From user_tables where upper(table_name) like '%CIVIL%';
+SELECT table_name From user_tables where upper(table_name) like '%FIDEL%';
+SELECT table_name From user_tables where upper(table_name) like '%BONIF%';
+SELECT table_name From user_tables where upper(table_name) like '%TIPO%';
+
+describe empleado;
+describe arriendo_moto;
+describe motocicleta;
+--- execution
+
+declare
+        cursor c_clientes is
+                select numrun_cli, dvrun_cli, nombre_cli
+                from cliente
+                order by numrun_cli asc;
+        
+        v_run cliente.numrun_cli%type;
+        v_dv cliente.dvrun_cli%type;
+        v_nombre cliente.nombre_cli%type;
+
+        v_cantidad_arriendo number;
+        v_total_dias number;
+        v_monto_total number;
+        v_puntaje number;
+        v_categoria varchar2 (20);
+
+        v_anno_processo constant number := 2025;
+begin
+        execute immediate 'truncate table scoring_cliente';
+
+        open c_clientes;
+        loop
+                fetch c_clientes
+                into v_run, v_dv, v_nombre;
+                exit when c_clientes%notfound;
+
+                v_cantidad_arriendo := 0;
+                v_total_dias := 0;
+                v_monto_total := 0;
+                v_puntaje := 0;
+                v_categoria := 'Nuevo';
+
+                select nvl(count(*), 0)
+                into v_cantidad_arriendo
+                from arriendo_moto
+                where arriendo_moto.numrun_cli = v_run
+                and extract(year from arriendo_moto.fecha_ini_arriendo) = v_anno_processo;
+
+                if v_cantidad_arriendo > 0 then
+                        select nvl(sum(arriendo_moto.dias_solicitados),0)
+                        into v_total_dias
+                        from arriendo_moto
+                        where arriendo_moto.numrun_cli = v_run
+                        and extract(year from arriendo_moto.fecha_ini_arriendo) = v_anno_processo;
+
+                        select nvl(sum(arriendo_moto.dias_solicitados * motocicleta.valor_arriendo_dia),0)
+                        into v_monto_total
+                        from arriendo_moto
+                        join motocicleta on arriendo_moto.placa = motocicleta.placa
+                        where arriendo_moto.numrun_cli = v_run
+                        and extract(year from arriendo_moto.fecha_ini_arriendo) = v_anno_processo;
+
+                        v_puntaje := round(v_cantidad_arriendo*10 + v_total_dias*3 + v_monto_total/50000);
+                end if;
+
+                if v_puntaje >= 150 then
+                        v_categoria := 'Oro';
+                elsif v_puntaje >= 60 then
+                        v_categoria := 'Plata';
+                elsif v_puntaje >= 1 then
+                        v_categoria := 'Bronce';
+                else
+                        v_categoria := 'Nuevo';
+                end if;
+
+                insert into scoring_cliente(
+                        numrun_cli,
+                        dvrun_cli,
+                        nombre_cli,
+                        cantidad_arriendo,
+                        total_dias,
+                        monto_total,
+                        puntaje,
+                        categoria_fidelidad
+                ) values (
+                        v_run,
+                        v_dv,
+                        v_nombre,
+                        v_cantidad_arriendo,
+                        v_total_dias,
+                        v_monto_total,
+                        v_puntaje,
+                        v_categoria
+                );
+        end loop;
+        close c_clientes;
+
+end;
+/
